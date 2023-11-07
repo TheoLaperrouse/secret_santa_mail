f3fa8788fce435390b32132e302d537949a59ccb
Théo Laperrouse <theo.laperrouse@energiency.com>
2023-11-07 01:14:06 +0100

    Update with google OAuth

--- README.md
+++ README.md
@@ -1,25 +1,16 @@
 # Secret Santa Mail
 
-## Mise en place
-
-### Récupération Mot de passe d'application Google
-
-1. Allez sur gérer mon compte google
-2. Sous "Se connecter à Google", vérifiez que la "vérification en deux étapes" est activée pour le compte.
-3. Toujours sous "Connexion à Google", sélectionnez "Mots de passe de l'application".
-4. Sélectionnez l'application "Mail" et l'appareil "Autre (nom personnalisé)" et donnez-lui un nom.
-5. Copiez le mot de passe de l'application, il se trouve dans un encart jaune et ressemble à ceci : "XXXX XXXX XXXX XXXX XXXX", coller le ensuite dans la variable EMAIL_PASSWORD du .env et renseigner l'adresse
-
-### Récupération de la libraire python-dotenv
-
-Installer les dépendances à partir du requirements.txt :
-```bash
-pip3 install -r requirements.txt
-```
-
 ## Utilisation
 
-Définir les différents participants et leur adresse mail dans user_infos.json
+1. Définir les différents participants et leur adresse mail dans user_infos.json
+2. Définir les paires à exclure dans is_valid_pairs
+2. Définir l'adresse mail utilisé pour envoyer les mails (MAIL dans .env)
+3. Récupération JSON OAuth Google API
+    1. Créer une application sur la console Google
+    2. Activer l'API gmail
+    3. Mettre en place l'OAuth
+    4. Créer un client OAuth
+    5. Récupérer le JSON et le mettre dans credentials.json
 
 Lancer ensuite le programme pour envoyer un email à tous les participants pour qu'il découvre à qui offrir un cadeau : 
 ```bash
--- secret_santa_mail.py
+++ secret_santa_mail.py
@@ -1,42 +1,91 @@
+from __future__ import print_function
+import os.path
+from email.message import EmailMessage
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 import random
 import json
-import smtplib
-import os
-from email.message import EmailMessage
+import base64
 from dotenv import load_dotenv
 
-
-def send_mail(sender,receiver,email_users):
-    """Send an email to the receiver
-
-    Args:
-        sender (str): name of the sender
-        receiver (str): name of the receiver
-        email_users (dict): email associated to name
-    """
-    _ = load_dotenv()
-    email_address = os.environ.get("EMAIL_ADDRESS")
-    email_password = os.environ.get("EMAIL_PASSWORD")
-    msg = EmailMessage()
-    msg['Subject'] = f"Bonjour {sender}, vous devez offrir un cadeau à {receiver}"
-    msg['To'] = email_users[receiver]
-    msg.set_content(f"Bonjour {sender}, vous devez offrir un cadeau à {receiver}.\nBon Secret Santa")
-    smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
-    smtpserver.starttls()
-    smtpserver.login(email_address, email_password)
-    smtpserver.send_message(msg)
+load_dotenv()
+SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
 
+def is_valid_pairs(pairs):
+    if len(pairs) == 0:
+        return False
+    EXCLUDED_PAIRS = [
+    ]
+    return all(
+        pair not in EXCLUDED_PAIRS and pair[::-1] not in EXCLUDED_PAIRS
+        for pair in pairs
+    )
 
-if __name__ == "__main__":
-    infos = open('user_infos.json', encoding='utf-8')
-    user_infos = json.load(infos)
+
+def get_pairs():
     pairs = []
     names = list(user_infos.keys())
     random.shuffle(names)
     names_shifted = names[1:] + names[:1]
     for index, name in enumerate(names):
         pairs.append([name, names_shifted[index]])
-    for pair in pairs: 
-        send_mail(pair[0], pair[1], user_infos)
+    return pairs
 
+
+def send_mail(sender, receiver, email_users, service):
+    with open("template.html", "r", encoding="utf-8") as file:
+        template_html = file.read()
+
+    template_html = template_html.replace("{{ sender }}", sender).replace(
+        "{{ receiver }}", receiver
+    )
+
+    msg = EmailMessage()
+    msg[
+        "Subject"
+    ] = f"Père Noël Secret : découvrez de qui vous êtes le Père Noël secret"
+    msg["To"] = email_users[receiver]
+    msg.set_content(template_html, subtype="html")
+
+    raw_message = base64.urlsafe_b64encode(msg.as_string().encode("utf-8")).decode(
+        "utf-8"
+    )
+
+    try:
+        service.users().messages().send(
+            userId=os.environ.get("EMAIL"), body={"raw": raw_message}
+        ).execute()
+        print("Email bien envoyé")
+    except HttpError as error:
+        print(f"Failed to send the email: {error}")
+
+
+if __name__ == "__main__":
+    creds = None
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build("gmail", "v1", credentials=creds)
+        pairs = []
+        infos = open("user_infos.json", encoding="utf-8")
+        user_infos = json.load(infos)
+        while not is_valid_pairs(pairs):
+            pairs = get_pairs()
+        for pair in pairs:
+            send_mail(pair[0], pair[1], user_infos, service)
+    except HttpError as error:
+        print(f"An error occurred: {error}")
